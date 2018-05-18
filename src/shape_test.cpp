6d59d887b855e47676cc81f7734e1a62ab47be4f
Tim Hutton <tihutt@microsoft.com>
2018-05-19 00:05:01 +0100

    Added restriction that new impacts must be to the right of the old ones. Gives something closer to the LLRR cardioid but not right. Added optimized profile for single-impact system.

--- src/shape_test.cpp
+++ src/shape_test.cpp
@@ -26,9 +26,21 @@ const double measured_llrr_radial[181]={1,1.003365683,1.005434206,1.00684812,1.0
 
 const double measured_random_walk_radial[181]={1,1.144048402,1.268977768,1.355015768,1.432079075,1.479793297,1.525271588,1.55423251,1.590896719,1.621708065,1.646383837,1.665225882,1.684681529,1.704469732,1.721676977,1.736309592,1.751267846,1.763710444,1.773660405,1.783414891,1.795142355,1.806374321,1.817120841,1.827360284,1.837069607,1.846248419,1.854926749,1.863155028,1.870983719,1.8784446,1.885544142,1.892272931,1.898626711,1.904627791,1.910333845,1.915826199,1.921180375,1.926432496,1.931559426,1.936484456,1.941107072,1.945342758,1.949154752,1.952565721,1.95564876,1.958506293,1.961247901,1.963974198,1.966767708,1.969687425,1.972763357,1.975990278,1.979323837,1.982684182,1.985970443,1.989084244,1.991954765,1.994555668,1.996907219,1.999063529,2.001091019,2.00304664,2.00496284,2.006842666,2.008665074,2.010398128,2.012015691,2.013511741,2.01490642,2.016240882,2.017562999,2.018910548,2.020299579,2.021722418,2.023154431,2.024564661,2.025925092,2.027215994,2.028428037,2.029563148,2.030634848,2.0316667,2.032686713,2.033717252,2.034763229,2.035803707,2.036791401,2.037661037,2.038343268,2.038778766,2.038928444,2.038778766,2.038343268,2.037661037,2.036791401,2.035803707,2.034763229,2.033717252,2.032686713,2.0316667,2.030634848,2.029563148,2.028428037,2.027215994,2.025925092,2.024564661,2.023154431,2.021722418,2.020299579,2.018910548,2.017562999,2.016240882,2.01490642,2.013511741,2.012015691,2.010398128,2.008665074,2.006842666,2.00496284,2.00304664,2.001091019,1.999063529,1.996907219,1.994555668,1.991954765,1.989084244,1.985970443,1.982684182,1.979323837,1.975990278,1.972763357,1.969687425,1.966767708,1.963974198,1.961247901,1.958506293,1.95564876,1.952565721,1.949154752,1.945342758,1.941107072,1.936484456,1.931559426,1.926432496,1.921180375,1.915826199,1.910333845,1.904627791,1.898626711,1.892272931,1.885544142,1.8784446,1.870983719,1.863155028,1.854926749,1.846248419,1.837069607,1.827360284,1.817120841,1.806374321,1.795142355,1.783414891,1.773660405,1.763710444,1.751267846,1.736309592,1.721676977,1.704469732,1.684681529,1.665225882,1.646383837,1.621708065,1.590896719,1.55423251,1.525271588,1.479793297,1.432079075,1.355015768,1.268977768,1.144048402,1}; // [0,180] degrees, measured using walk_plot.cpp
 
+const double optimized_radial_profile1[181]={1.119892,1.204358,1.243752,1.275329,1.316589,1.339737,1.367579,1.381811,1.406476,1.427300,1.444576,1.458801,1.476194,1.496018,1.513411,1.530104,1.547660,1.564191,1.578858,1.593857,1.611609,1.628264,1.643955,1.659654,1.674888,1.690567,1.705922,1.721633,1.736862,1.752180,1.766416,1.781353,1.795995,1.810730,1.825225,1.839432,1.853230,1.866803,1.879609,1.893324,1.907331,1.920541,1.934923,1.948833,1.961966,1.975785,1.989534,2.002617,2.014701,2.028075,2.041010,2.054182,2.067259,2.079669,2.091302,2.101426,2.109606,2.116288,2.121368,2.125062,2.128130,2.130261,2.132611,2.134659,2.136631,2.138887,2.140769,2.142546,2.143931,2.145152,2.145871,2.147948,2.148739,2.150101,2.151241,2.152093,2.152551,2.153084,2.153603,2.154324,2.155067,2.155732,2.156541,2.156865,2.157557,2.157451,2.157466,2.157482,2.157461,2.157418,2.157461,2.157418,2.157461,2.157482,2.157466,2.157451,2.157557,2.156865,2.156541,2.155732,2.155067,2.154324,2.153603,2.153084,2.152551,2.152093,2.151241,2.150101,2.148739,2.147948,2.145871,2.145152,2.143931,2.142546,2.140769,2.138887,2.136631,2.134659,2.132611,2.130261,2.128130,2.125062,2.121368,2.116288,2.109606,2.101426,2.091302,2.079669,2.067259,2.054182,2.041010,2.028075,2.014701,2.002617,1.989534,1.975785,1.961966,1.948833,1.934923,1.920541,1.907331,1.893324,1.879609,1.866803,1.853230,1.839432,1.825225,1.810730,1.795995,1.781353,1.766416,1.752180,1.736862,1.721633,1.705922,1.690567,1.674888,1.659654,1.643955,1.628264,1.611609,1.593857,1.578858,1.564191,1.547660,1.530104,1.513411,1.496018,1.476194,1.458801,1.444576,1.427300,1.406476,1.381811,1.367579,1.339737,1.316589,1.275329,1.243752,1.204358,1.119892}; // [0,180] degrees, output of this program
+
 bool inMoore(int x,int y,int tx,int ty) { return abs(x-tx)<=1 && abs(y-ty)<=1; }
 bool onGrid(int x,int y,int X,int Y) { return x>=0 && x<X && y>=0 && y<Y; }
 int dist2(int x,int y,int sx,int sy) { return (x-sx)*(x-sx)+(y-sy)*(y-sy); }
+bool onLeft(int x,int y,int x1,int y1,int x2,int y2) {
+    if (y1 == y2 && x1 == x2) { return false; } // zero-length line
+    float perp_x = float(y1-y2); // -dy
+    float perp_y = float(x2-x1); // dx
+    float lp = hypot(perp_x,perp_y);
+    perp_x /= lp;
+    perp_y /= lp;
+    float dot = perp_x * (x-x1) + perp_y * (y-y1);
+    return dot > 2;
+}
 
 int main()
 {
@@ -48,12 +60,13 @@ int main()
     struct Impact { int x, y; double n; };
     std::vector<Impact> impacts;
 
-    std::vector<double> radial(std::begin(measured_random_walk_radial), std::end(measured_random_walk_radial));
+    std::vector<double> radial(std::begin(optimized_radial_profile1), std::end(optimized_radial_profile1));
 
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<> random_move(0, N_DIRS - 1);
-
+    
+    const bool for_cardioid = true;
     for (int iAdjustment = 0; ; iAdjustment++)
     {
         // initialize chain from radial
@@ -68,7 +81,7 @@ int main()
         for (int theta = 0; theta <= 180; theta++)
         {
             const double rad = theta * PI / 180.0;
-            const double r = X / 4 * radial[theta];
+            const double r = X / 6 * radial[theta];
             int bx = std::clamp((int)round(sx + r * cos(rad)), 0, X - 1);
             int by = std::clamp((int)round(sy + r * sin(rad)), 0, Y - 1);
             if (theta > 0) {
@@ -85,20 +98,25 @@ int main()
 
         // draw the chain for debugging
         std::ostringstream oss;
-        oss << "shape_test_chain_" << std::setfill('0') << std::setw(4) << iAdjustment << ".bmp";
+        oss << "shape_test_chain2_" << std::setfill('0') << std::setw(4) << iAdjustment << ".bmp";
         write_bmp(oss.str(), X, Y, [&](int x, int y, unsigned char bgr[3]) {
-            if (chain_idx[y][x] > -1) { bgr[0] = bgr[1] = bgr[2] = 255; }
+            if( (x==sx && y==sy) || (chain_idx[y][x] > -1) ) { bgr[0] = bgr[1] = bgr[2] = 255; }
             else { bgr[0] = bgr[1] = bgr[2] = 0; }
         });
+        std::cout << "Wrote " << oss.str() << std::endl;
         
         if(iAdjustment==20) break;
 
         auto t1 = std::chrono::high_resolution_clock::now();
 
+        int iChain_last_impact = (int)impacts.size();
+
         uint64_t num_impacts = 0;
         uint64_t iterations = 0;
-        const uint64_t expected_impacts = 50;
-        for (; iterations < 100 * billion && num_impacts < expected_impacts * impacts.size(); ++iterations)
+        const uint64_t expected_impacts = 100;
+        x = sx;
+        y = sy;
+        for (; iterations < 10*billion && num_impacts < expected_impacts * impacts.size(); ++iterations)
         {
             int dir = random_move(gen);
             int new_x = x + dx[dir];
@@ -108,27 +126,39 @@ int main()
                 // start afresh
                 x = sx;
                 y = sy;
+                if(for_cardioid) {
+                    iChain_last_impact = (int)impacts.size();
+                }
                 continue;
             }
-            /*if (!onGrid(new_x, new_y, X, Y)) // DEBUG
-            {
-                std::cerr << "Moved off grid" << std::endl;
-                exit(EXIT_FAILURE);
-            }*/
             int iChain = chain_idx[new_y][new_x];
             if (iChain >= 0) {
-                // make a note of the impact and start afresh
+                if(iChain>=iChain_last_impact) {
+                    // hit the curved wall to the left of a previous impact,
+                    // ignore this move and continue
+                    continue;
+                }
+                // make a note of the impact
                 impacts[iChain].n++;
                 num_impacts++;
-                x = sx;
-                y = sy;
-                continue;
+                
+                if(for_cardioid) {
+                    // record the new impact point and continue from here without taking the step
+                    iChain_last_impact = iChain;
+                }
+                else {
+                    // start afresh
+                    x = sx;
+                    y = sy;
+                    continue;
+                }
             }
             else {
                 // take the step
                 x = new_x;
                 y = new_y;
             }
+
         }
 
         // output the impact counts to console
@@ -168,9 +198,11 @@ int main()
                 const int itheta = (int)round(theta);
                 acc[itheta].val += i.n;
                 acc[itheta].n++;
-                // make symmetrical
-                acc[180-itheta].val += i.n;
-                acc[180-itheta].n++;
+                if(!for_cardioid) {
+                    // make symmetrical
+                    acc[180-itheta].val += i.n;
+                    acc[180-itheta].n++;
+                }
             }
             for(int theta=0;theta<=180;theta++)
             {
